03400c4dbfc875d8aef5e9edbfcbba6682308de0
Andrew Westenskow <andrew.westenskow@gmail.com>
2019-04-09 15:28:38 -0600

    Finished black diamond

--- black-diamond/src/Components/StoreFront/StoreFront.js
+++ black-diamond/src/Components/StoreFront/StoreFront.js
@@ -3,8 +3,9 @@ import "./StoreFront.css";
 
 class StoreFront extends Component {
   render() {
+
     let productDisplay = this.props.products.map((element, index) => {
-      <div className="product-container" key={index}>
+      return <div className="product-container" key={index}>
         <h2>{element.title}</h2>
         <img src={element.image} alt="" />
         <h2>{element.desc}</h2>
@@ -12,7 +13,10 @@ class StoreFront extends Component {
         <button onClick={() => this.props.addToCart(element)}>Purchase!</button>
       </div>;
     });
-    return <div className="storefront-container">{productDisplay}</div>;
+
+    return (
+      <div className="storefront-container">{productDisplay}</div>
+    );
   }
 }
 
